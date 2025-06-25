3a8680deb95f37ac2b1bf86a17e2acfc66f95df7
William Fernandes <wiilfern1910@outlook.com>
2025-06-25 08:42:24 -0300

    feat: Criação URL Repository

---
+++ src/main/java/com/liamfer/urlShortener/repository/URLRepository.java
@@ -0,0 +1,10 @@
+package com.liamfer.urlShortener.repository;
+
+import com.liamfer.urlShortener.domain.URLEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface URLRepository extends JpaRepository<URLEntity,Long>{
+    URLEntity findByshortCode(String shortCode);
+}
